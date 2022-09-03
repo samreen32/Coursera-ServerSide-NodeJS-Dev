30cf46e9ac0546234606f9ba0e0d03339bfde6a0
Samreen <iamsamreenk@gmail.com>
2022-09-03 16:17:40 +0500

    Simple Node Example-Area-of-Rectangle

---
+++ index.js
@@ -0,0 +1,23 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect (l,b){
+    console.log("Solving for rectangle with length = " + l + " and breath = " + b);
+
+    if (l <= 0 || b <= 0) {
+        console.log("Rectangle dimensions should be greater than zero:  length = "
+               + l + ",  and breath = " + b);
+    }
+    else {
+	    console.log("The area of the rectangle is " + rect.area(l,b));
+	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
+    }
+}
+
+//calling the function and providing values for length and breath.
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
